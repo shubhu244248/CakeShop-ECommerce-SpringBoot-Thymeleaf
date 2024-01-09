d794e27a045422e275937a89caa9dcdf28d0f75b
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-01-09 15:04:09 +0530

    Project initialization and the package creation

---
+++ CakeShop_E-Commerce_SpringBoot/src/main/java/com/cakeshop/CakeShopECommerceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cakeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeShopECommerceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeShopECommerceSpringBootApplication.class, args);
+	}
+
+}
