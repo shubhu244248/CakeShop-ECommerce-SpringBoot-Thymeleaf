aa9623a80ff36ce180fb7e5e1dc7677448396dfb
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-01-23 11:31:25 +0530

    Service methods added and the custom one service for the find all product

--- CakeShop_E-Commerce_SpringBoot/src/main/java/com/cakeshop/service/ProductService.java
+++ CakeShop_E-Commerce_SpringBoot/src/main/java/com/cakeshop/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.cakeshop.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,22 @@ public class ProductService {
 
 	public List<Product> getAllProducts() {
 		return productRepository.findAll();
+	}
+	
+	public void addProduct(Product product ) {
+		productRepository.save(product);
+	}
+	
+	public void removeProductById(long id ) {
+		productRepository.deleteById(id);
+	}
+	
+	public Optional<Product> getProductById(long id) {
+		return productRepository.findById(id);
+	}
+	
+	public List<Product> getAllProductByCategoryId(int id) {
+		return productRepository.findAllByCategory_Id(id);
 	}
 	
 	
